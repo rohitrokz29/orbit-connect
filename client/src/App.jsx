a8aee34c309f866f5d6e0a6eda6b07abb04f29f9
Rohit Kharche <111701144+rohitrokz29@users.noreply.github.com>
2024-01-24 14:24:22 +0530

    connected
    Connected socket rooms for client and server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,85 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import socketIO from 'socket.io-client'
 
 
-const socket = socketIO.connect('http://localhost:3000')
+const socket = socketIO.connect('http://localhost:3000/')
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [heading, setHeading] = useState("Vite")
+  const [organiser, setOrganiser] = useState('');
+  const [room, setroom] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
+  const [error, setError] = useState("");
+  const [msg, setMsg] = useState("");
   useEffect(() => {
-    socket.on('io:recieved_message',(data)=>{
-      setHeading(data['msg'])
+
+    socket.on('room:create', ({ room, password }) => {
+      setPassword(password);
+      setroom(room);
+      console.log({ organiser, room, password, user, error, msg })
     })
+    socket.on('room:start', (data) => {
+      setMsg(data['message']);
+
+      console.log({ organiser, room, password, user, error, msg })
+
+    })
+    socket.on('room:new_user', (data) => {
+      setMsg(`${data['user']} joined`)
+
+      console.log({ organiser, room, password, user, error, msg })
+    })
+    socket.on('room:error', (data) => setError(data['message']))
+    socket.on("room:message",({message,user})=>{
+      setMsg(message+user);
+      console.log({message,user})
+    })
+    return () => {
+
+      socket.off('room:create', ({ room, password }) => {
+        setPassword(password);
+        setroom(room);
+      })
+      socket.off('room:start', (data) => {
+        setMsg(data['message']);
+      })
+      socket.off('room:new_user', (data) => {
+        setMsg(`${data['user']} joined`)
+      })
+    }
   }, [])
 
   return (
     <>
-     
-      <h1>{heading}</h1>
-      <div className="card">
-        <button onClick={() => {
-          setCount((count) => count + 1)
-          socket.emit('io:send_message', { msg: "hello Its rohit" })
-        }
-        }>
-        </button>
-      </div>
+      {msg}
+      <input type="text" name="" id="" value={organiser} placeholder='organiser' onChange={(e) => setOrganiser(e.target.value)} />
+      <input type="text" name="" id="" value={room} placeholder='room' onChange={(e) => setroom(e.target.value)} />
+      <input type="text" name="" id="" value={password} placeholder='password' onChange={(e) => setPassword(e.target.value)} />
+      <input type="text" name="" id="" value={user} placeholder='user' onChange={(e) => setUser(e.target.value)} />
+    
+      {error && <div>{error}</div>}
+      <button
+        onClick={() => {
+          socket.emit('room:create', { userId: organiser });
+          console.log({ organiser, room, password, user, error, msg })
+
+        }}
+      >Create Rooom</button>
+      <button onClick={() => {
+        socket.emit('room:start', { room, organiser, password });
+        console.log({ organiser, room, password, user, error, msg })
+
+      }}>Start Meet</button>
+      <button onClick={() => {
+        socket.emit('room:join', { user, room, password })
+        console.log({ organiser, room, password, user, error, msg })
+
+      }}>Join Meet</button>
+
+  <input type="text" name="" id="" value={msg} placeholder='message' onChange={(e) => setMsg(e.target.value)} />
+      <button type="button" onClick={()=>{
+        socket.emit('room:message',{room,message:msg,user});
+      }}>Send Message</button>
     </>
   )
 }
--- server/Server.js
+++ server/Server.js
@@ -1,25 +1,41 @@
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
+const { Connection } = require('./socketMethods/Connection');
+const { urlencoded } = require('express');
+const bodyParser = require('body-parser');
 
 const app = require('express')();
 app.use(require('cors')())
+app.use(urlencoded());
 
-const socketServer=createServer(app);
-const io=new Server(socketServer,{
-    cors:{origin:'http://localhost:5173'}
+app.use(bodyParser.json());
+const socketServer = createServer(app);
+const io = new Server(socketServer, {
+    cors: { origin: 'http://localhost:5173' }
 });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     // console.log(io)
-    res.json({socketServer});
+    res.json({ socketServer });
 })
-io.on('connection',(socket)=>{
-    console.log('connected to socket');
-    socket.on('io:send_message',(data)=>{
-        console.log(data);
-        socket.emit('io:recieved_message',{msg:"recieved message"})
+
+var roomId="ABC123";
+var password="Rohit";
+io.on('connection', socket=>{
+    console.log(socket.id);
+    socket.on('room:start',({room,organiser,password})=>{
+        socket.join(room);
+        socket.broadcast.to(room).emit("room:start",{message:"Meet Started"});
     })
-})
-socketServer.listen(3000,()=>{
+    socket.on('room:join',({room,password,user})=>{
+        socket.join(room);
+        socket.broadcast.to(room).emit("room:new_user",{user})
+    })
+    socket.on('room:message',({room,message,user})=>{
+        socket.broadcast.to(room).emit('room:message',{message,user})
+    })
+});
+
+socketServer.listen(3000, () => {
     console.log('Running at port 3000');
 })
