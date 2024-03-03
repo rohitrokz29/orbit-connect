e351b5f3267dc51fd491e982acd329171883c428
Rohit Kharche <r.s.kharche0098@gmail.com>
2024-03-03 11:30:47 +0530

    updated home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,24 +1,63 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
+import GlobeImage from "../assets/images/globe.jpeg";
+import ConnectionImage from "../assets/images/securecon.jpeg";
+import MessageImage from "../assets/images/livemessage.jpeg";
+import DataImage from "../assets/images/dataSec.jpeg";
+import "./styles/home.css";
 const Home = () => {
   const { user, isSignedIn } = useContext(UserContext);
+  const features = [
+    { name: "Connect Globally", image: GlobeImage },
+    { name: "Secure Connection", image: ConnectionImage },
+    { name: "Live Messaging", image: MessageImage },
+    { name: "Data Security", image: DataImage },
+  ];
   return (
     <>
-    <main>
-      <div className="top-box">
-        <div className='heading'>
-          <span className="heading-text">
-            Join us and connect with your close ones throughout the globe and share valuable time together.
-          </span>
-          <div className="buttons">
-            <Link className="button " to="/signup">Get Started</Link>
-            <Link className="button " to="/signin">Already a User</Link>
+      <main>
+        <div className="top-box">
+          <div className="heading">
+            <span className="heading-text">
+              <Link to={"/"} className="brand">
+                Orbit Connect
+              </Link>
+              <br />
+              Join us at the earliest and connect with your close ones
+              throughout the globe and share valuable time together.
+            </span>
+            <div className="buttons">
+              <Link className="button " to="/signup">
+                Get Started
+              </Link>
+              <Link className="button " to="/signin">
+                Already a User
+              </Link>
+            </div>
           </div>
+          <SideImage/>
         </div>
-      </div>
-    </main>
+        <div className="features">
+          {features.map((property) => {
+            return (
+              <div className="prop-card" key={`${property.name}`}>
+                <img className="prop-image" src={property.image} />
+                <span className="prop-name">{property.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </>
   );
 };
 export default Home;
+
+ export const SideImage = () => {
+  return (
+    <div className="side-image">
+      <img className="image" src={GlobeImage} />
+    </div>
+  );
+};
